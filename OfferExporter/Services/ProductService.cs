69169f7092625d22f160bebbc9284734699c60dd
victorprouff <victorprouff@outlook.fr>
2022-09-11 12:08:48 +0200

    WIP

--- OfferExporter/Services/ProductService.cs
+++ OfferExporter/Services/ProductService.cs
@@ -4,45 +4,9 @@ public class ProductService
 {
     public List<Product> BuildProducts(List<GetAllOffersResultRow> rows)
     {
-        var products = new List<Product>();
+        var products = rows.Select(CreateProduct).Distinct().ToList();
 
-        for (int m = 0; m < rows.Count; m++)
-        {
-            var row = rows[m];
-
-            var product = new Product
-            {
-                Prid = row.ProductPrid,
-                ReferentialId = row.ReferentialId,
-                ReferentialName = row.ReferentialName
-            };
-
-            // 
-            var found = products.FirstOrDefault(p => product.Equals(p));
-            if (found is not null)
-            {
-                continue;
-            }
-
-            products.Add(product);
-
-            // 
-            product.Offers.Add(new Offer
-            {
-                Id = row.OfferId,
-                Company = row.SellerName,
-                Price = row.OfferPrice,
-                ReducedPrice = row.PromotionReducedPrice,
-                Quantity = row.OfferQuantity,
-                DiscountFor = row.PromotionTargetName
-            });
-
-            // 
-            if (m == rows.Count - 1)
-            {
-                products.Add(product);
-            }
-        }
+        products.Add(products.Last());
 
         // Quick-fix: Sort the products and offers (so the generate file is always the same)
         products = products
@@ -60,5 +24,26 @@ public class ProductService
         Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fff")} Convert data into products: {products.Count} products");
 
         return products;
+    }
+
+    private static Product CreateProduct(GetAllOffersResultRow row)
+    {
+        var product = new Product
+        {
+            Prid = row.ProductPrid,
+            ReferentialId = row.ReferentialId,
+            ReferentialName = row.ReferentialName
+        };
+
+        product.Offers.Add(new Offer
+        {
+            Id = row.OfferId,
+            Company = row.SellerName,
+            Price = row.OfferPrice,
+            ReducedPrice = row.PromotionReducedPrice,
+            Quantity = row.OfferQuantity,
+            DiscountFor = row.PromotionTargetName
+        });
+        return product;
     }
 }
