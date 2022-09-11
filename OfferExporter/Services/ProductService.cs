a66ec1c3589650aa4d0d10457dd1d5d0892b69bc
victorprouff <victorprouff@outlook.fr>
2022-09-11 13:05:23 +0200

    wip

--- OfferExporter/Services/ProductService.cs
+++ OfferExporter/Services/ProductService.cs
@@ -4,6 +4,64 @@ public class ProductService
 {
     public List<Product> BuildProducts(List<GetAllOffersResultRow> rows)
     {
+        /* // var products = rows.Distinct().Select(CreateProduct).ToList();
+
+         // foreach (var row in rows.Distinct())
+         // {
+         //     var product = CreateProduct(row);
+         //
+         //     products.Add(product);
+         // }
+
+         foreach (var t in rows.Distinct())
+         {
+             var product = CreateProduct(t);
+
+             // 
+             if (products.Exists(p => p.Equals(product)))
+             {
+                 continue;
+             }
+
+             products.Add(product);
+         }
+
+         products.Add(products.Last()); */
+
+        // Test
+        var products = new List<Product>();
+
+        foreach (var p in rows.GroupBy(r => new { r.ProductPrid, r.ReferentialId }))
+        {
+            Product? product = null;
+
+            foreach (var row in p)
+            {
+                product ??= new Product(row.ProductPrid, row.ReferentialId, row.ReferentialName);
+
+                product.Offers.Add(new Offer
+                {
+                    Id = row.OfferId,
+                    Company = row.SellerName,
+                    Price = row.OfferPrice,
+                    ReducedPrice = row.PromotionReducedPrice,
+                    Quantity = row.OfferQuantity,
+                    DiscountFor = row.PromotionTargetName
+                });
+            }
+
+            if (product != null)
+            {
+                products.Add(product);
+            }
+        }
+
+        if (products.Count > 0)
+        {
+            products.Add(products.Last());
+        }
+
+        /*
         var products = new List<Product>();
 
         for (var m = 0; m < rows.Count; m++)
@@ -36,7 +94,7 @@ public class ProductService
             {
                 products.Add(product);
             }
-        }
+        }*/
 
         // Quick-fix: Sort the products and offers (so the generate file is always the same)
         products = products
