f8ccb122ad9ec2f70daaa7e95b668e62da0a403e
victor prouff <victorprouff@outlook.fr>
2022-09-11 13:10:34 +0200

    Clean

--- OfferExporter/Services/ProductService.cs
+++ OfferExporter/Services/ProductService.cs
@@ -4,31 +4,6 @@ public class ProductService
 {
     public List<Product> BuildProducts(List<GetAllOffersResultRow> rows)
     {
-        /* // var products = rows.Distinct().Select(CreateProduct).ToList();
-
-         // foreach (var row in rows.Distinct())
-         // {
-         //     var product = CreateProduct(row);
-         //
-         //     products.Add(product);
-         // }
-
-         foreach (var t in rows.Distinct())
-         {
-             var product = CreateProduct(t);
-
-             // 
-             if (products.Exists(p => p.Equals(product)))
-             {
-                 continue;
-             }
-
-             products.Add(product);
-         }
-
-         products.Add(products.Last()); */
-
-        // Test
         var products = new List<Product>();
 
         foreach (var p in rows.GroupBy(r => new { r.ProductPrid, r.ReferentialId }))
@@ -60,41 +35,6 @@ public class ProductService
         {
             products.Add(products.Last());
         }
-
-        /*
-        var products = new List<Product>();
-
-        for (var m = 0; m < rows.Count; m++)
-        {
-            var row = rows[m];
-
-            var product = new Product(row.ProductPrid, row.ReferentialId, row.ReferentialName);
-
-            var found = products.FirstOrDefault(p => product.Equals(p));
-            if (found is not null)
-            {
-                product = found;
-            }
-            else
-            {
-                products.Add(product);
-            }
-
-            product.Offers.Add(new Offer
-            {
-                Id = row.OfferId,
-                Company = row.SellerName,
-                Price = row.OfferPrice,
-                ReducedPrice = row.PromotionReducedPrice,
-                Quantity = row.OfferQuantity,
-                DiscountFor = row.PromotionTargetName
-            });
-
-            if (m == rows.Count - 1)
-            {
-                products.Add(product);
-            }
-        }*/
 
         // Quick-fix: Sort the products and offers (so the generate file is always the same)
         products = products
