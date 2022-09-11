b837cd310b49624b6f83b32a347469c88d8d1bd6
victor prouff <victorprouff@outlook.fr>
2022-09-11 14:45:08 +0200

    Optimize RowService

--- OfferExporter/Services/RowService.cs
+++ OfferExporter/Services/RowService.cs
@@ -4,53 +4,16 @@ public class RowService
 {
     public List<GetAllOffersResultRow> RemoveInvalidRows(List<GetAllOffersResultRow> rows)
     {
-        for (var i = rows.Count - 1; i >= 0; i--)
-        {
-            var row = rows[i];
+        var result = rows.Where(r =>
+            r.OfferIsActive &&
+            r.OfferPrice > 0 &&
+            r.OfferQuantity > 0 &&
+            r.SellerIsActive &&
+            r.ReferentialIsExportable &&
+            (r.PromotionReducedPrice is null || r.PromotionReducedPrice is > 0)).ToList();
 
-            if (!row.OfferIsActive)
-            {
-                rows.Remove(row);
-                continue;
-            }
+        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fff")} Exclude invalid offers: {result.Count} left");
 
-            if (row.OfferPrice <= 0)
-            {
-                rows.Remove(row);
-                continue;
-            }
-
-            if (row.PromotionReducedPrice.HasValue)
-            {
-                if (row.PromotionReducedPrice <= 0)
-                {
-                    rows.Remove(row);
-                    continue;
-                }
-            }
-
-            if (row.OfferQuantity <= 0)
-            {
-                rows.Remove(row);
-                continue;
-            }
-
-            // Filter on seller data
-            if (!row.SellerIsActive)
-            {
-                rows.Remove(row);
-            }
-
-            // Filter on referential data
-            if (!row.ReferentialIsExportable)
-            {
-                rows.Remove(row);
-                continue;
-            }
-        }
-
-        Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss.fff")} Exclude invalid offers: {rows.Count} left");
-
-        return rows;
+        return result;
     }
 }
